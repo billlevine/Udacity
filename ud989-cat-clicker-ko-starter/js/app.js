990b18cdc31452f6dc78304e10f2e37ef7477893
Bill LeVine <blevine@ieee.org>
2015-06-19 00:33:25 -0400

    multiple cats

--- ud989-cat-clicker-ko-starter/js/app.js
+++ ud989-cat-clicker-ko-starter/js/app.js
@@ -1,24 +1,32 @@
-function Cat(name,image) {
-	this.name = name;
-	this.image = image;
-	this.count = 0;
-};
+var initialCats = [
+		{	name: "Bill",
+			imgSrc: "./img/cat1.jpg",
+			imgAttribution: "http://www.crestron.com",
+			nickNames:["billy", "bilbo", "will", "willy", "shitter"]	},
+		{	name: "Jason",
+			imgSrc: "./img/cat2.jpg",
+			imgAttribution: "http://www.crestron.com",
+			nickNames:["billy", "bilbo", "will", "willy", "shitter"]	},
+		{	name: "Jill",
+			imgSrc: "./img/cat4.jpg",
+			imgAttribution: "http://www.crestron.com",
+			nickNames:["billy", "bilbo", "will", "willy", "shitter"]	},
+		{	name: "Criag",
+			imgSrc: "./img/cat5.jpg",
+			imgAttribution: "http://www.crestron.com",
+			nickNames:["billy", "bilbo", "will", "willy", "shitter"]	},
+		{	name: "Tippy",
+			imgSrc: "./img/cat6.jpg",
+			imgAttribution: "http://www.crestron.com",
+			nickNames:["billy", "bilbo", "will", "willy", "shitter"]	},
+];
 
-
-function ViewModel() {
-
-	//this.cats = ko.observableArray([]);
-	//this.cats.push( new Cat("bill", "./img/cat1.jpg") );
-	//this.cats.push( new Cat("bob", "./img/cat2.jpg") );
-
+function Cat(data) {
 	this.clickCount = ko.observable(0);
-	this.name = ko.observable("bill");
-	this.imgSrc = ko.observable("./img/cat1.jpg");
-	this.imgAttribution = ko.observable("http://www.flickr.com");
-
-	this.incrementCount = function() {
-		this.clickCount( this.clickCount() + 1 );
-		}
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgSrc);
+	this.nickNames = ko.observableArray(data.nickNames);
 
 	this.level = ko.computed( (function(_this) {
 		return function() {
@@ -30,6 +38,21 @@ function ViewModel() {
 			return "newborn";
 		}};
 		}) (this) );
+};
+
+function ViewModel() {
+	_this = this;
+	_this.catList = ko.observableArray([]);
+	initialCats.forEach( function(item) {
+		_this.catList.push( new Cat(item) ); } );
+	_this.currentCat = ko.observable(_this.catList()[0]);
+
+	_this.incrementCount = function() {
+		_this.currentCat().clickCount( _this.currentCat().clickCount() + 1 );
+		}
+	_this.catClicked = function() {
+		_this.currentCat(this);	
+		};
 }
 
 ko.applyBindings( new ViewModel() );
